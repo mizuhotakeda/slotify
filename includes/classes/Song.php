857f9b4ed58e58d5129af26b82d5f04decc47112
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-04-23 00:14:15 +0200

    Fix incorrect variable in Song.php

--- includes/classes/Song.php
+++ includes/classes/Song.php
@@ -15,13 +15,13 @@
         $this->id = $id;
 
         $query = mysqli_query($this->con, "SELECT * FROM songs WHERE id='$this->id'");
-        $this->$mysqliData = mysqli_fetch_array($query);
-        $this->title = $this->$mysqliData['title'];
-        $this->artistId = $this->$mysqliData['artist'];
-        $this->albumId = $this->$mysqliData['album'];
-        $this->genre = $this->$mysqliData['genre'];
-        $this->duration = $this->$mysqliData['duration'];
-        $this->path = $this->$mysqliData['path'];
+        $this->mysqliData = mysqli_fetch_array($query);
+        $this->title = $this->mysqliData['title'];
+        $this->artistId = $this->mysqliData['artist'];
+        $this->albumId = $this->mysqliData['album'];
+        $this->genre = $this->mysqliData['genre'];
+        $this->duration = $this->mysqliData['duration'];
+        $this->path = $this->mysqliData['path'];
       }
 
       public function getTitle() {
