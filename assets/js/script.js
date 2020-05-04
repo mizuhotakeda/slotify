3e5d3e13e6a95e284fcaf64f3bb305d7b59570ca
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-04 22:32:33 +0200

    95. Displaying the time remaining label

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,12 @@ function Audio() {
     this.currentlyPlaying;
     this.audio = document.createElement('audio'); // = php class
 
+    this.audio.addEventListener("canplay", function() {
+        //'this' refers to the object that the event was called on
+        $(".progressTime.remaining").text(this.duration);
+    });
+
+
     this.setTrack = function(track) { // = php public function, track instead of src
         this.currentlyPlaying = track;
         this.audio.src = track.path;
