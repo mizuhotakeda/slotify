8e269b04b48b39604059853025d75f20b64f3526
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-07 15:59:10 +0200

    107. Mute button

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -107,6 +107,12 @@
         $(".controlButton.repeat img").attr("src", "assets/images/icons/" + imageName);
     }
 
+    function setMute() {
+        audioElement.audio.muted =! audioElement.audio.muted;
+        var imageName = audioElement.audio.muted ? "volume-mute.png" : "volume.png";
+        $(".controlButton.volume img").attr("src", "assets/images/icons/" + imageName);
+    }
+
     function setTrack(trackId, newPlaylist, play) {
         currentIndex = currentPlaylist.indexOf(trackId);
         pauseSong();
@@ -218,7 +224,7 @@
 
         <div id="nowPlayingRight"> <!-- 下段右　-->
             <div class="volumeBar">
-                <button class="controlButton volume" title="Volume button">
+                <button class="controlButton volume" title="Volume button" onclick="setMute()">
                     <img src="assets/images/icons/volume.png" alt="Volume">
                 </button>
                 <div class="progressBar">
