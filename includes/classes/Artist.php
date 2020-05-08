046712940bfa967ffc823b9f2a3bfa7df9fb3465
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-08 20:06:58 +0200

    122. Songs section

--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -10,11 +10,21 @@
         $this->id = $id;
       }
 
-
       public function getName() {
         $artistQuery = mysqli_query($this->con, "SELECT name FROM artists WHERE id='$this->id'");
         $artist = mysqli_fetch_array($artistQuery);
         return $artist['name'];
+      }
+
+      public function getSongIds() { //Copied from Album.php
+        $query = mysqli_query($this->con, "SELECT id FROM songs WHERE artist='$this->id' ORDER BY plays DESC");
+        $array = array();
+
+        while($row = mysqli_fetch_array($query)) {
+          array_push($array, $row['id']);
+        }
+
+        return $array;
       }
   }
 ?>
