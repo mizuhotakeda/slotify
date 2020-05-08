6e152157d1e1ceace2392757aca602b9bb06194e
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-09 00:31:20 +0200

    123. Albums section

--- artist.php
+++ artist.php
@@ -26,6 +26,7 @@
 
 
 <div class="tracklistContainer borderBottom">
+    <h2>SONGS</h2>
     <ul class="tracklist">
 
         <?php
@@ -71,5 +72,24 @@
         </script>
 
     </ul>
+</div>
+
+<div class="gridViewContainer">
+    <h2>ALBUMS</h2>
+
+    <?php
+        $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE artist='$artistId'");
+        while($row = mysqli_fetch_array($albumQuery)) {
+
+          echo "<div class='gridViewItem'>
+                    <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
+                        <img src='" . $row['artworkPath']. "'>
+                        <div class='gridViewInfo'>"
+                            . $row['title'] .
+                        "</div>
+                    </span>
+                </div>";
+        }
+     ?>
 
 </div>
