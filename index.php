1a36b181b1a930a4b900471cfadab82811443b27
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-08 15:45:49 +0200

    119. Browse page

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
-include("includes/includedFiles.php");
+  include("includes/includedFiles.php");
 ?>
 
-  <h1 class="pageHeadingBig">You Might Also Like</h1>
-
-  <div class="gridViewContainer">
-
-      <?php
-          $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
-          while($row = mysqli_fetch_array($albumQuery)) {
-
-            // '" "'（シングル・クォーテーション）（ダブル・クォーテーション）にすることで、前と後に分解する
-            //（ドット） .  .内で呼び出すrowの定義
-            echo "<div class='gridViewItem'>
-                      <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
-                          <img src='" . $row['artworkPath']. "'>
-                          <div class='gridViewInfo'>"
-                              . $row['title'] .
-                          "</div>
-                      </span>
-                  </div>";
-          }
-       ?>
-
-  </div>
+<script>
+  openPage("browse.php")  
+</script>
