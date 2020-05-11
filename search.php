ac92ac3c69dce430738d4a8d2c97e9efc3589db0
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-11 13:52:42 +0200

    133. Searching for albums

--- search.php
+++ search.php
@@ -32,6 +32,8 @@
 
 </script>
 
+<?php if($term == "") exit(); ?>
+
 <div class="tracklistContainer borderBottom">
     <h2>SONGS</h2>
     <ul class="tracklist">
@@ -108,7 +110,31 @@
                       </div>
                   </div>";
         }
-
     ?>
+
+</div>
+
+<div class="gridViewContainer">
+    <h2>ALBUMS</h2>
+
+    <?php
+        $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE title LIKE '$term%' LIMIT 10");
+
+        if(mysqli_num_rows($albumQuery) == 0) {
+            echo "<span class='noResults'>No albums found matching " . $term . "</span>";
+        }
+
+        while($row = mysqli_fetch_array($albumQuery)) {
+
+          echo "<div class='gridViewItem'>
+                    <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
+                        <img src='" . $row['artworkPath']. "'>
+                        <div class='gridViewInfo'>"
+                            . $row['title'] .
+                        "</div>
+                    </span>
+                </div>";
+        }
+     ?>
 
 </div>
