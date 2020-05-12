e2fbc9471417f72a22cc566afd686071a13e6675
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-12 14:29:52 +0200

    139. Creating the User class

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -2,9 +2,18 @@
 
   if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) { //ajax request
     include("includes/config.php");
+    include("includes/classes/User.php"); //should be coded before Artist/Album/Songs.php
     include("includes/classes/Artist.php");
     include("includes/classes/Album.php");
     include("includes/classes/Song.php");
+
+    if(isset($_GET['userLoggedIn'])) {
+        $userLoggedIn = new User($con, $_GET['userLoggedIn']);
+    } else {
+        echo "Username variable was not passed into page. Please check the openPage JS function.";
+        exit();
+    }
+
   } else {
     include("includes/header.php");
     include("includes/footer.php");
