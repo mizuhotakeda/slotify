644c0178f38fddaefd191aa2135abd76cf7db422
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-13 14:38:09 +0200

    147. Delete playlist ajax

--- playlist.php
+++ playlist.php
@@ -22,7 +22,7 @@
         <h2><?php echo $playlist->getName(); ?></h2>
         <p>By <?php echo $playlist->getOwner(); ?></p>
         <p><?php echo $playlist->getNumberOfSongs(); ?> songs</p>
-        <button class="button" onclick="deletePlaylist(1)">DELETE PLAYLIST</button>
+        <button class="button" onclick="deletePlaylist('<?php echo $playlistId; ?>')">DELETE PLAYLIST</button>
     </div>
 </div>
 
