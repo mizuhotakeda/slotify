a26f53510e7a056e1e1be39d666c3e5656a96073
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-13 14:24:40 +0200

    146. Delete playlist function

--- playlist.php
+++ playlist.php
@@ -22,7 +22,7 @@
         <h2><?php echo $playlist->getName(); ?></h2>
         <p>By <?php echo $playlist->getOwner(); ?></p>
         <p><?php echo $playlist->getNumberOfSongs(); ?> songs</p>
-        <button class="button">DELETE PLAYLIST</button>
+        <button class="button" onclick="deletePlaylist(1)">DELETE PLAYLIST</button>
     </div>
 </div>
 
