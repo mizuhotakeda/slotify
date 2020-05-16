79542700d34918d6c8df28ece9351f378a084f1b
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-16 15:37:58 +0200

    150. Hiding the options menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,17 @@ var shuffle = false;
 var userLoggedIn;
 var timer;
 
+$(document).click(function(click) {
+    var target = $(click.target);
+
+    if(!target.hasClass("item") && !target.hasClass("optionsButton")) {
+        hideOptionsMenu();
+    }
+});
+
+$(window).scroll(function() {
+    hideOptionsMenu();
+});
 
 function openPage(url) {
     if(timer != null) {
@@ -60,6 +71,13 @@ function deletePlaylist(playlistId) {
             //do something when ajax returns
             openPage("yourMusic.php");
         });
+    }
+}
+
+function hideOptionsMenu() {
+    var menu = $(".optionsMenu");
+    if(menu.css("display") != "none") {
+        menu.css("display", "none");
     }
 }
 
