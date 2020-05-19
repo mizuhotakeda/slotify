35c9481157dc7dac7d50aacc1f29edd64a9b9758
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-19 13:56:08 +0200

    Bug fixed

--- album.php
+++ album.php
@@ -23,7 +23,7 @@
     </div>
     <div class="rightSection">
         <h2><?php echo $album->getTitle(); ?></h2>
-        <p role="link" tabindex="0" onclick="openPage('artist.php?id=$artistId')">By <?php echo $artist->getName(); ?></p>
+        <p role="link" tabindex="0" onclick="openPage('artist.php?id=<?php echo $artist->getId(); ?>')">By <?php echo $artist->getName(); ?></p>
         <p><?php echo $album->getNumberOfSongs(); ?> songs</p>
     </div>
 </div>
