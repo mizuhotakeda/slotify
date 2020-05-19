ce99a5b495ab16bde8b0520a220269df06e45245
mizuhotakeda <32306596+mizuhotakeda@users.noreply.github.com>
2020-05-19 11:44:41 +0200

    165. Update email function

--- updateDetails.php
+++ updateDetails.php
@@ -7,8 +7,8 @@
     <div class="container borderBottom">
         <h2>EMAIL</h2>
         <input type="text" class="email" name="email" placeholder="Email address..." value="<?php echo $userLoggedIn->getEmail(); ?>">
-        <span class="message">Email updated!</span>
-        <button class="button" onclick="">SAVE</button>
+        <span class="message"></span>
+        <button class="button" onclick="updateEmail('email')">SAVE</button>
     </div>
 
     <div class="container">
